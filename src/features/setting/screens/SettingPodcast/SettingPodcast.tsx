21cdbd5bcc2c0d0658aa5789d90b4bc6f1a9da97
sonle <sonweb2001@gmail.com>
2024-05-13 22:12:30 +0700

    FE: UI list podcast

--- src/features/setting/screens/SettingPodcast/SettingPodcast.tsx
+++ src/features/setting/screens/SettingPodcast/SettingPodcast.tsx
@@ -7,7 +7,13 @@ import {
 } from "@app/features/setting/setting";
 
 import TitleSetting from "../../components/TitleSetting/TitleSetting";
-import { HeaderSetting } from "./SettingPodcast.styles";
+import {
+  HeaderSetting,
+  Table,
+  WrapAction,
+  WrapTable,
+  WrapTile,
+} from "./SettingPodcast.styles";
 
 const SettingPodcast: FC = () => {
   return (
@@ -16,24 +22,85 @@ const SettingPodcast: FC = () => {
 
       <HeaderSetting>
         <AppButton
-          variant="text"
+          size="small"
           to={`${SettingPathsEnum.SETTINGS}/${SettingSubPathsEnum.CREATE_PODCAST}`}
         >
           Create
         </AppButton>
       </HeaderSetting>
-      <table>
-        <thead>
-          <tr>
-            <th>STT</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-          </tr>
-        </tbody>
-      </table>
+
+      <WrapTable>
+        <Table>
+          <thead>
+            <tr>
+              <th>
+                <input type="checkbox" />
+              </th>
+              <th>STT</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Topic</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>1</td>
+              <td>
+                <WrapTile>
+                  <img
+                    src="https://cdn.pixabay.com/photo/2019/09/02/10/00/lemon-4446934_640.jpg"
+                    alt=""
+                  />
+                  <p>Green World environmental protection</p>
+                </WrapTile>
+              </td>
+              <td>Mr. Son</td>
+              <td>Environment</td>
+              <td>
+                <WrapAction>
+                  <AppButton variant="outlined" size="small">
+                    Delete
+                  </AppButton>
+                  <AppButton variant="outlined" size="small">
+                    Edit
+                  </AppButton>
+                </WrapAction>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>2</td>
+              <td>
+                <WrapTile>
+                  <img
+                    src="https://cdn.pixabay.com/photo/2024/04/20/11/47/ai-generated-8708404_640.jpg"
+                    alt=""
+                  />
+                  <p>Green World environmental protection</p>
+                </WrapTile>
+              </td>
+              <td>Mr. Son</td>
+              <td>Environment</td>
+              <td>
+                <WrapAction>
+                  <AppButton variant="outlined" size="small">
+                    Delete
+                  </AppButton>
+                  <AppButton variant="outlined" size="small">
+                    Edit
+                  </AppButton>
+                </WrapAction>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </WrapTable>
     </>
   );
 };
