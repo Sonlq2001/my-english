20809dd6550c4c287bfdf3410b89a515d5227070
sonle <sonweb2001@gmail.com>
2024-07-10 23:15:12 +0700

    FE: UI list popular speaking

--- src/features/speaking/constants/speaking.constants.ts
+++ src/features/speaking/constants/speaking.constants.ts
@@ -1,26 +1,39 @@
 import { VocabularyPathsEnum } from "@app/features/vocabulary/vocabulary";
 
-import ThumbnailOne from "@app/assets/images/voice-assistant.png";
-import ThumbnailTwo from "@app/assets/images/voice-assistant-amico.png";
-import ThumbnailThree from "@app/assets/images/voice-assistant-pana.png";
+// import ThumbnailOne from "@app/assets/images/voice-assistant.png";
+// import ThumbnailTwo from "@app/assets/images/voice-assistant-amico.png";
+// import ThumbnailThree from "@app/assets/images/voice-assistant-pana.png";
 
 import { TABS_ACTIONS_READING } from "./speaking.enum";
 import { TypesTabsActions } from "../types/speaking.types";
 
 export const LIST_SPEAKING_STYLE = [
   {
-    title: "Vocabulary pronunciation",
-    thumbnail: ThumbnailOne,
+    title: "Vocabulary",
+    subTitle: "Pronunciation according to vocabulary",
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2024/03/27/14/01/ai-generated-8659195_640.jpg",
     path: VocabularyPathsEnum.LIST_VOCABULARY_TOPICS,
   },
   {
-    title: "Practice your voice with phrases and short sentences",
-    thumbnail: ThumbnailTwo,
+    title: "Short text",
+    subTitle: "Sample sentences and examples",
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2020/09/23/02/01/microphone-5594702_1280.jpg",
     path: "/speaking/phrases",
   },
   {
     title: "Voice to text",
-    thumbnail: ThumbnailThree,
+    subTitle: "Practice your voice with phrases and short sentences",
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2024/02/13/16/24/podcast-8571329_640.jpg",
+    path: "/voice-to-text",
+  },
+  {
+    title: "Voice to text 2",
+    subTitle: "Practice your voice with phrases and short sentences",
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2024/06/06/20/06/singer-8813370_640.png",
     path: "/voice-to-text",
   },
 ];
--- src/features/speaking/screens/SpeakingScreen/SpeakingScreen.styles.ts
+++ src/features/speaking/screens/SpeakingScreen/SpeakingScreen.styles.ts
@@ -1,8 +1,62 @@
 import { styled } from "styled-components";
 
-export const WrapSpeaking = styled.div`
+export const WrapSpeaking = styled.section`
+  padding-top: 3.5rem;
+`;
+
+export const InnerSpeaking = styled.section`
+  border-radius: 2.5rem;
+  background-color: #fff;
+  padding: 2.5rem 3rem;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`;
+
+export const ListTypes = styled.div`
   display: flex;
-  gap: 2.4rem;
-  padding: 2rem;
-  background-color: ${(props) => props.theme.palette.white};
+  gap: 2.5rem;
+  margin-top: 3.5rem;
+`;
+
+export const ItemTypeSpeaking = styled.article`
+  width: calc(100% / 4);
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgb(120 14 14 / 50%);
+  }
+  img {
+    width: 100%;
+    max-height: 25rem;
+    height: 100%;
+  }
+  .content-item {
+    padding: 2.5rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    color: ${(props) => props.theme.palette.white};
+    p {
+      flex: 1;
+      line-height: 2rem;
+    }
+    .btn-join {
+      background-color: ${(props) => props.theme.palette.white};
+      border: none;
+      border-radius: 2rem;
+      margin-left: auto;
+    }
+  }
 `;
